82f2adb3d8bfe2dd27d0c7ea63c98703df5801c6
Vignesh Kumar Subramanian <vmani@us.ibm.com>
2019-03-05 23:58:58 -0500

    iterative inorder traversal

---
+++ InterviewPrep/src/Dfs/IterativeInorderTraversal.java
@@ -0,0 +1,33 @@
+package Dfs;
+
+import java.util.List;
+
+public class IterativeInorderTraversal {
+	/**
+	 * Definition for a binary tree node.
+	 * public class TreeNode {
+	 *     int val;
+	 *     TreeNode left;
+	 *     TreeNode right;
+	 *     TreeNode(int x) { val = x; }
+	 * }
+	 */
+	public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<Integer>();
+
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        TreeNode cur = root;
+
+        while(cur!=null || !stack.empty()){
+            while(cur!=null){
+                stack.add(cur);
+                cur = cur.left;
+            }
+            cur = stack.pop();
+            list.add(cur.val);
+            cur = cur.right;
+        }
+
+        return list;
+    }
+}
